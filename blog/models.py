9154136d961466d149b6ada77bf7c78e9bd65751
Willie Tran <tran.william26@gmail.com>
2014-10-02 17:30:31 -0700

    Added modeling and for loop templates

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=30)
+    post = models.TextField()
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from blog.models import BlogPost
+
 
 def home(request):
-    return render(request, 'main.html')
+    blog = BlogPost.objects.all()
+    blog_posts = { "blogs" : blog }
+    return render(request, 'main.html', blog_posts)
